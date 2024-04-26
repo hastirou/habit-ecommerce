0eacb9e5f2cd174864698994d39bd8f7f67164d1
ihastiro <ihastiro@gmail.com>
2024-04-27 01:30:52 +0700

    first

---
+++ js/products/categories/bags.js
@@ -0,0 +1,150 @@
+/* jshint undef: true, unused: true */
+/* global define: false */
+define([], function(){
+	return {
+		name: "Bags",
+		oneRow: false,
+		imagesPath: "images/",
+		addCodePath: false,
+		items: [
+			{
+				name: "Saltnic Snap! 25mg",
+				price: {
+					current: "120.000"
+				},
+				code: "N8101",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates odit vitae repellat doloremque dolorem, numquam.",
+				thumb: "product1.png",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "Pod x Signature - Blue",
+				price: {
+					current: "315.000",
+					old: "330.000"
+				},
+				code: "N8102",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, animi quam obcaecati.",
+				thumb: "product2.webp",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "Pod x Signature - Beige",
+				price: {
+					current: "315.000",
+					old: "330.000"
+				},
+				code: "N8103",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "product3.webp",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "Makna Berry Dough 7mg",
+				price: {
+					current: "140.000",
+					old: "150.000"
+				},
+				code: "N8104",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "product6.jpg",
+				gallery: ["460x590", "460x590", "460x590"],
+			},
+			{
+				name: "Pod x Signature - Green",
+				price: {
+					current: "315.000",
+					old: "330.000"
+				},
+				code: "N8105",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "product4.webp",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "Free Base Snap! 3mg",
+				price: {
+					current: "125.000"
+				},
+				code: "N8106",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "product5.png",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "Pod X - Space Grey",
+				price: {
+					current: "170.000",
+					old: "190.000"
+				},
+				code: "N8107",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "product7.webp",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "Pod X - Absolute Black",
+				price: {
+					current: "170.000",
+					old: "190.000"
+				},
+				code: "N8108",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "product8.webp",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "Pod X - Hot Pink",
+				price: {
+					current: "170.000",
+					old: "190.000"
+				},
+				code: "N8109",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "product9.webp",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "Foom Refillable Catrige X",
+				price: {
+					current: "100.000",
+				},
+				code: "N8110",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "product10.webp",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "Mod HEXOHM V3",
+				price: {
+					current: "3.500.000"
+				},
+				code: "N8112",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "product11.jpg",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "Argus Pro",
+				price: {
+					current: "325.000",
+					old: "380.000"
+				},
+				code: "N8113",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "product12.png",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "Argus GT",
+				price: {
+					current: "300.000"
+				},
+				code: "N8114",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "product13.png",
+				gallery: ["460x590", "460x590", "460x590"]
+			}
+		]
+	};
+});
